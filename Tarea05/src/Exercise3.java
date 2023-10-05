e1cb3e9e04d93d95c6f77c6289ec437cc0d803d8
ivandavid858 <ivandavid858@gmail.com>
2023-10-05 10:24:11 +0200

    Correction made in Exercise 3

---
+++ Tarea05/src/Exercise3.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+
+		String name = JOptionPane.showInputDialog("Ingresa tu nombre");
+		JOptionPane.showMessageDialog(null, "Bienvenido " + name);		
+
+	}
+
+}
