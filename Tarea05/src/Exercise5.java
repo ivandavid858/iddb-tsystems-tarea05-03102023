560aa3da6d9d3812f1961115f43bf393aebb6d94
ivandavid858 <ivandavid858@gmail.com>
2023-10-05 10:38:27 +0200

    Exercise 5 done

---
+++ Tarea05/src/Exercise5.java
@@ -0,0 +1,16 @@
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+
+		int num = 5;
+		
+		if (num % 2 == 0) {
+			System.out.println(num + " es divisible entre 2");
+		} else {
+			System.out.println(num + " no es divisible entre 2");
+		}
+
+	}
+
+}
