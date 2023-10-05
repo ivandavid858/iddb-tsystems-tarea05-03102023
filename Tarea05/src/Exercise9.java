dfe759a1d8d779fd6b3f04ef1da5dbe8ae7973b2
ivandavid858 <ivandavid858@gmail.com>
2023-10-05 11:47:10 +0200

    Exercise 9 done

---
+++ Tarea05/src/Exercise9.java
@@ -0,0 +1,17 @@
+
+public class Exercise9 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while (i <= 100) {
+			if (i % 2 == 0 || i % 3 == 0) {
+				System.out.println(i);
+			}
+			i++;
+		}
+
+	}
+
+}
