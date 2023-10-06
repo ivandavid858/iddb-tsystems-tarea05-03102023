39ddbf8e5a610961089d2b2e8e36b359793090f2
ivandavid858 <ivandavid858@gmail.com>
2023-10-06 13:56:42 +0200

    Exercise 11 done

--- Tarea05/src/Exercise11.java
+++ Tarea05/src/Exercise11.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class Exercise11 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Ingresar un día de la semana:");
+		Scanner sc = new Scanner(System.in);
 		
+		String day = sc.nextLine();
+		
+		switch (day) {
+			case "lunes":
+			case "martes":
+			case "miercoles":
+			case "jueves":
+			case "viernes":
+				System.out.println("El día " + day + " es un día laboral");
+				break;
+			case "sabado":
+			case "domingo":
+				System.out.println("El día " + day + " no es un día laboral");
+				break;
+			default:
+				System.out.println("Ingresar día de la semana correcto");
+				break;
+		}
+		
+		sc.close();
 
 	}
 
